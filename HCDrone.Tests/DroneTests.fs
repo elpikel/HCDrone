ee640ea16a64521662974b175f7c4a6f0a031eb2
Arkadiusz Plichta <Arkadiusz.Plichta@powel.com>
2016-09-06 15:37:16 +0200

    fsunit instead of fscheck

--- HCDrone.Tests/DroneTests.fs
+++ HCDrone.Tests/DroneTests.fs
@@ -2,14 +2,19 @@
 
 open AR.Drone.Client
 open Xunit
+open FsUnit.Xunit
 open System.Threading
 
 [<Fact>]
 let ``Should Take Off and Land`` () =
     use drone = new DroneClient("192.168.1.1")
+    
     drone.Start()
     drone.Takeoff()
+    
     Thread.Sleep(2000)
+    
     drone.Land()
     drone.Stop()
-    ()
+    
+    1 |> should equal 1
